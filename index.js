c6cafdbcb54c4dbd73b6634f4f0d78b398f91754
emily-cheung1 <emilycheung777@gmail.com>
2022-03-10 18:05:24 -0800

    fixed decimal

--- index.js
+++ index.js
@@ -71,10 +71,10 @@ const runComparison = () => {
 
     leftSideStats.forEach((leftStat, index) => {
         const rightStat = rightSideStats[index];
-        const leftSideValue = parseInt(leftStat.dataset.value);
-        const rightSideValue = parseInt(rightStat.dataset.value);
+        const leftSideValue = parseFloat(leftStat.dataset.value);
+        const rightSideValue = parseFloat(rightStat.dataset.value);
 
-        if (rightSideValue > leftSideValue) {
+        if ( rightSideValue > leftSideValue) {
             leftStat.classList.remove('is-primary');
             leftStat.classList.add('is-warning');
             rightStat.classList.add('is-primary');
